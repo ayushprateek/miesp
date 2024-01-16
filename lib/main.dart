b41b1befcad213ba7503c15344b65c6a649354e9
ayushprateek <ayushpratik08041999@gmail.com>
2024-01-17 00:36:01 +0530

    Re,oved unused dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,12 @@
-import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:miesp/local_storage/local_storage.dart';
 import 'package:miesp/miesp.dart';
 import 'package:miesp/theme/custom_theme.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
-  await FlutterDownloader.initialize(
-      debug: false, // optional: set to false to disable printing logs to console (default: true)
-      ignoreSsl: true // option: set to false to disable working with http links (default: false)
-  );
 
   init();
 
@@ -25,6 +19,5 @@ Future<void> main() async {
 }
 
 Future<void> init() async {
-
   await LocalStorage.getInstance()?.initLocalStorage();
 }
